f46deb3d256122a65604076dd556dad795fd949b
mohankumaronly <mohankumaronly81@gmail.com>
2026-04-24 19:03:25 +0530

    .env file loaded into the main file

--- src/main/java/com/ranger/jobtracker/JobtrackerApplication.java
+++ src/main/java/com/ranger/jobtracker/JobtrackerApplication.java
@@ -1,5 +1,6 @@
 package com.ranger.jobtracker;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +8,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JobtrackerApplication {
 
 	public static void main(String[] args) {
+
+		Dotenv dotenv = Dotenv.load();
+
+		System.setProperty("DB_URL", dotenv.get("DB_URL"));
+		System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+
 		SpringApplication.run(JobtrackerApplication.class, args);
 	}
-
 }
