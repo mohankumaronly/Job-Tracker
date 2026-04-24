f31a1c74b052f4bea9f415a2a5642aa416b559a4
mohankumaronly <mohankumaronly81@gmail.com>
2026-04-24 18:57:40 +0530

    project is setting up

---
+++ src/test/java/com/ranger/jobtracker/JobtrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ranger.jobtracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobtrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
